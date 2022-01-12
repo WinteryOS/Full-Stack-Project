bce4ed857fa14ea5d713957dcb129f060636fe2d
John <jashby@student.neumont.edu>
2022-01-12 08:48:23 -0700

    eslint updated

--- full-stack-project/client/.eslintrc.js
+++ full-stack-project/client/.eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
+  extends: ["eslint:recommended", "plugin:vue/recommended"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
@@ -11,4 +11,6 @@ module.exports = {
   parserOptions: {
     parser: "babel-eslint",
   },
+  "vue/singleline-html-element-content-newline": "off",
+  "vue/multiline-html-element-content-newline": "off",
 };
--- full-stack-project/server/controllers/user_controller.js
+++ full-stack-project/server/controllers/user_controller.js
@@ -1,16 +1,16 @@
-'use strict';
+"use strict";
 
-const User = require('../models/user_schema');
+const User = require("../models/user_schema");
 
 const createData = (req, res) => {
   User.create(req.body)
     .then((data) => {
-      console.log('New User Created!', data);
+      console.log("New User Created!", data);
       res.status(201).json(data);
     })
     .catch((err) => {
-      if (err.name === 'ValidationError') {
-        console.error('Error Validating!', err);
+      if (err.name === "ValidationError") {
+        console.error("Error Validating!", err);
         res.status(422).json(err);
       } else {
         console.error(err);
@@ -36,12 +36,12 @@ const updateData = (req, res) => {
     new: true,
   })
     .then((data) => {
-      console.log('User updated!');
+      console.log("User updated!");
       res.status(201).json(data);
     })
     .catch((err) => {
-      if (err.name === 'ValidationError') {
-        console.error('Error Validating!', err);
+      if (err.name === "ValidationError") {
+        console.error("Error Validating!", err);
         res.status(422).json(err);
       } else {
         console.error(err);
@@ -54,12 +54,12 @@ const deleteData = (req, res) => {
   User.findById(req.params.id)
     .then((data) => {
       if (!data) {
-        throw new Error('User not available');
+        throw new Error("User not available");
       }
       return data.remove();
     })
     .then((data) => {
-      console.log('User removed!');
+      console.log("User removed!");
       res.status(200).json(data);
     })
     .catch((err) => {
